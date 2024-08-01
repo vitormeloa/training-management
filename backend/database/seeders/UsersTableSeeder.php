c7cb1da6d26e8a1582a60c34b8469b29513bb41b
vitormelo.a <melo.vitor2001@gmail.com>
2024-08-01 06:13:09 -0300

    feat: adding Users Table seeder

--- backend/database/seeders/UsersTableSeeder.php
+++ backend/database/seeders/UsersTableSeeder.php
@@ -2,16 +2,14 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        User::factory(10)->create();
     }
 }
+
